eb03b27b53e490eed07939701d14eff61432cc46
Crisbm9 <cristinab.tandem@gmail.com>
2024-09-12 13:08:26 +0200

    primero

---
+++ src/components/PageFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function PageFooter() {
+  return (
+    <div id="footer">
+      <ul className="copyright">
+        <li>CRISTINA BECERRA MORENO CV</li>
+        
+      </ul>
+    </div>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,195 @@
+import React from 'react';
+import "./index.css"
+
+import Layout from '../components/Layout';
+import PageFooter from '../components/PageFooter';
+import SideBar from '../components/SideBar';
+
+import Scroll from '../components/Scroll';
+
+
+const sections = [
+  { id: 'portfolio', name: 'Formación académica' },
+  { id: 'about', name: 'Experiencia laboral' },
+  { id: 'ao', name: 'Programación' },
+  { id: 'al', name: 'Idiomas' },
+  { id: 'smr', name: 'Ejemplos de páginas hechas' },
+  { id: 'ro', name: 'Aptitudes' },
+  
+
+];
+
+const IndexPage = () => (
+  <Layout>
+    <SideBar sections={sections} />
+
+    <div id="main">
+      <section id="top" className="one dark cover">
+        <div className="container">
+          <header>
+            <h2 className="alt">
+              CRISTINA BECERRA MORENO
+              <br />
+              
+            </h2>
+            
+            <p>
+               
+              
+            </p>
+            
+          </header>
+
+          <footer>
+            <Scroll type="id" element={'portfolio'}>
+              <a href="#portfolio" className="button">
+                Saber más
+              </a>
+            </Scroll>
+          </footer>
+        </div>
+      </section>
+
+      <section id="portfolio" className="two">
+        <div className="container">
+          <header>
+            <h2>FORMACIÓN ACADÉMICA </h2>
+          </header>
+          
+          <br></br>
+          <p>
+          Certificado de Profesionalidad de DESARROLLO DE APLICACIONES CON TECNOLOGÍA WEB (IFCD0210) (2023-2024)
+            </p>
+            
+            <p>
+            Bachillerato de ciencias 
+            </p>
+            
+            {/* <h3 style={{color:`#e2bb8f`}}>
+
+            </h3> */}
+            
+          
+
+          {/* <div className="row">
+            <div className="col-4 col-12-mobile">
+              <article className="item">
+                
+                <header>
+                  <h3>Ipsum Feugiat</h3>
+                </header>
+              </article>
+              <article className="item">
+              
+                <header>
+                  <h3>Rhoncus Semper</h3>
+                </header>
+              </article>
+            </div>
+            <div className="col-4 col-12-mobile">
+              <article className="item">
+                
+                <header>
+                  <h3>Magna Nullam</h3>
+                </header>
+              </article>
+              <article className="item">
+                
+                <header>
+                  <h3>Natoque Vitae</h3>
+                </header>
+              </article>
+            </div>
+            <div className="col-4 col-12-mobile">
+              <article className="item">
+                
+                <header>
+                  <h3>Dolor Penatibus</h3>
+                </header>
+              </article>
+              <article className="item">
+                
+                <header>
+                  <h3>Orci Convallis</h3>
+                </header>
+              </article>
+            </div>
+          </div> */}
+        </div>
+      </section>
+
+      <section id="about" className="three">
+        <div className="container">
+          <header>
+            <h2>EXPERIENCIA LABORAL</h2>
+          </header>
+
+          
+           <p>
+          <h3 style={{color:`#e2bb8f`}}>Desarrollo web para Patrimonio Nacional (1 año) desarrollando aplicaciones y páginas web </h3>            
+            </p>
+            <p>
+            Aprovisionamiento y reposición en el departamento de logística de Ikea (4 meses)
+            </p>
+            <p>
+            Dependienta en Primark de Gran Vía (3 meses). 
+            </p>
+            <p>
+            Monitora de gimnasio y recepcionista en el Holiday Gym (6 meses).
+            </p>
+            <p>
+            Croupier en El casino Gran Vía de Madrid (6 meses).
+            </p>
+            <p>
+            Secretaria para la empresa de limpieza Marber (3 meses).
+            </p>
+            <p>
+            Profesora particular de clases de matemáticas, física, química y dibujo técnico de nivel ESO y bachiller. 
+            </p>
+        </div>
+      </section>
+
+      
+
+      <section id="ao" className="three">
+        <div className="container">
+          <header>
+            <h2>PROGRAMACIÓN </h2>
+          </header>
+
+          
+          <p>
+          TECNOLOGÍAS Y LENGUAJES DE PROGRAMACIÓN: HTML, CSS, JS, PHP, MySQL, Json, Markdown, xml, graphql.
+          </p>
+            <p>
+            HERRAMIENTAS DE DESARROLLO Y GESTIÓN DE PROYECTOS : VSCode, Git, Github, Figma, FileZilla, Netlify. 
+            </p>
+            <p>
+            LIBRERÍAS Y FRAMEWORKS: Bootstrap, Gatsby, Wordpress, React. 
+            </p>
+            
+        </div>
+      </section>
+      
+      <section id="al" className="three">
+        <div className="container">
+          <header>
+            <h2>IDIOMAS </h2>
+          </header>
+
+          
+          <p>
+          Inglés B1
+          </p>
+            
+            
+        </div>
+      </section>
+      
+    </div>
+
+    <PageFooter />
+  </Layout>
+);
+
+export default IndexPage;
